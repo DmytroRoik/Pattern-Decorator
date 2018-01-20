d18f3e7faa58f16b36d67eca8fa0b8abe73d9b83
DmytroRoik <dmytroroik@gmail.com>
2018-01-21 00:41:17 +0200

    add output in console

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 var u=new User();
 var du=new DecorateUser(u);
-console.log(du.getDiscount());
+console.log('decorate User',du);
+console.log('discount',du.getDiscount());
+console.log('bonus',du.getBonus());
